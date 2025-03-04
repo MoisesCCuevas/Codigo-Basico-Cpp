d239279a3f63183a807390669739ae285c0420c6
Moisés C. Cuevas <moisesccuevas@gmail.com>
2025-03-04 17:34:35 -0600

    update math demo

--- mathDemo.c
+++ mathDemo.c
@@ -2,10 +2,13 @@
 #include <math.h>
 
 int x;
+float result;
 
 int main() {
     printf("Ingrese el valor de x:");
     scanf("%d", &x);
+    result = sin(x);
+    printf("El seno de x es: %f\n", result);
     printf("El valor de x es: %d\n", x);
     return 0;
 }
